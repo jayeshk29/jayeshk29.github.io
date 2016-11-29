235b568c72f941cd02dae4fd1306564e81c78f15
Jayesh Kaushik <jayeshk@berkeley.edu>
2016-11-28 21:57:00 -0800

    First commit
    Added files for landing page.

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+ $("#modal-container").hide();
+	
+	// Question 4
+
+$(".modalfooter").click(function() {
+ $("#modal-container").hide();
+});
+
+$("#link").click(function() {
+ $("#modal-container").show();
+});
+
+$("#modal-overlay").click(function() {
+ $("#modal-container").hide();
+});
+	// IMPLEMENT "SHOW MODAL" WHEN "CLICK ON LOGIN BUTTON FROM MAIN PAGE" HERE
+
+	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON SUBMIT BUTTON FROM MODAL BOX" HERE
+
+	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON CANCEL BUTTON FROM MODAL BOX" HERE
+	
+	
+	// Question 5
+	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON MODAL OVERLAY" HERE
+	
+});
